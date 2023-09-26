4b74ec5cad683caadeab45d0db1e951b3d976ce5
nickfff-dev <hunchographics@gmail.com>
2023-09-27 01:00:21 +0300

    shell sort

--- sort.h
+++ sort.h
@@ -25,4 +25,5 @@ void quick_sort(int *array, size_t size);
 void lomuto_sort(int *array, int low, int high, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void swap(int *a, int *b);
+void shell_sort(int *array, size_t size);
 #endif
